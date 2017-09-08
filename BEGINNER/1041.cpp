da9af5cf1fb31554eaeca2215076506fba56bf75
karen-lopez <stefanny@utp.edu.co>
2017-09-08 18:37:13 -0500

    new

---
+++ BEGINNER/1041.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+
+int main(void){
+
+  double X, Y;
+  scanf("%lf %lf", &X, &Y);
+
+  if(X == Y && Y == 0) printf("Origem\n");
+  if(Y==0 && X != 0) printf("Eixo X\n");
+  if(X==0 && Y != 0) printf("Eixo Y\n");
+  if(X > 0 && Y > 0) printf("Q1\n");
+  if(X < 0 && Y > 0) printf("Q2\n");
+  if(X < 0 && Y < 0) printf("Q3\n");
+  if(X > 0 && Y < 0) printf("Q4\n");
+
+  return 0;
+
+}
